5e3c4da35c9f2c56ee8789f7426249844662c809
Allen Abad <84323269+yowallen@users.noreply.github.com>
2023-05-30 09:52:00 +0800

    finished login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import loginPic from "../assets/12345.jpg"
 import '.././index.css'
+import loginPic from "../assets/12345.jpg"
+import loginLogo from "../assets/mcloginlogo.png"
 
 const Login = () => {
   return (
     <div className='bgImg h-screen flex'>
-        <div className='glass px-24 basis-1/2 flex flex-col justify-center'>
+        <div className='glass px-28 basis-1/2 flex flex-col justify-center'>
             <div className='py-8'>
-                <h1 className='text-gray-700 font-pop font-bold text-4xl py-4 text-center'>Welcome Back!</h1>
+                <h1 className='text-gray-700 font-pop font-bold text-4xl py-4 text-center'>Glad to See Your Smile Back!</h1>
                 <p className='font-lora font-semibold text-center text-lg text-gray-700'>"MC Dentals: Where Healthy Smiles Shine Bright!"</p>
             </div>
             <form className='text-gray-700'>
@@ -26,6 +27,9 @@ const Login = () => {
                 </button>
             </form>
             <p className='text-gray-700 font-pop font-semibold py-8'>Don't have an account? <a href="#" className='text-blue-950 hover:underline'>Register Here.</a></p>
+            <a href="/">
+                <img src={loginLogo} alt="mc login logo" className='w-96 max-w-full mx-auto p-8' style={{mixBlendMode: "multiply"}}/>
+            </a>
         </div>
 
         <div className='basis-1/2'>
