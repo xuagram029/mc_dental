33fbe11b92eec202d5725c17bb815b5bf17eb740
Allen Abad <84323269+yowallen@users.noreply.github.com>
2023-05-31 23:20:24 +0800

    Update Contacts.jsx

--- client/src/components/Landing Page/Contacts.jsx
+++ client/src/components/Landing Page/Contacts.jsx
@@ -7,16 +7,25 @@ import {AiOutlineUser, AiOutlineMail} from "react-icons/ai"
 export const Contacts = () => {
   return (
     <div className='h-full bg-rose-300 rounded-md flex sm:flex-col lg:flex-row'>
-      <div className='basis-1/3 bg-primary m-2 rounded-xl px-8 lg:pt-24 sm:pt-8 border-2 border-primary'>
-        <div>
+      <div className='basis-1/3 bg-primary lg:my-28 sm:my-0 lg:ml-10 rounded-md sm:order-2 lg:order-1'>
+        <h1 className='text-2xl font-pop text-white font-semibold text-center py-6 px-14'>Site Map of Our MC Dental Clinic Branches</h1>
+        <div className='flex items-center justify-center'>
+          <iframe width="350" height="350" frameborder="10" scrolling="no" marginheight="0" marginwidth="auto" src="https://www.google.com/maps/d/u/2/embed?mid=1Cfo3U3CA0XbgSr5YDve5LiIu5RvHFNM&ehbc=2E312F">
+          </iframe>
+        </div>
+        
+      </div>
+
+      <div className='basis-2/3 bg-rose-300 flex flex-col items-center sm:pl-4 lg:my-32 sm:my-0 sm:order-1 lg:order-2'>
+      <div>
           <h1 className='font-pop text-white font-medium text-3xl'>Contact Information</h1>
-          <p className='font-pop text-white font-extralight py-4'>Fill up the form and Our Team will get back to you within 24 hours.</p>
+          <p className='font-pop text-white font-extralight py-8'>Fill up the form and Our Team will get back to you within 24 hours.</p>
         </div>
 
-        <div className='space-y-12 lg:pt-16 sm:pt-4 font-pop'>
+        <div className='space-y-8 font-pop'>
           <div className='flex flex-col gap-y-3 text-white'>
             <span className='flex items-center gap-x-8 text-xl'>
-              <HiLocationMarker className='text-lg text-rose-300' />
+              <HiLocationMarker className='text-lg text-white' />
               <p className=' text-base font-medium'>Branch 1: 2341 Leveriza St., Pasay City, Philippines</p>
             </span>
 
@@ -29,14 +38,14 @@ export const Contacts = () => {
 
           <div className='flex items-center gap-x-8 text-white'>
             <span>
-              <BsFillTelephoneFill className='text-rose-300' />
+              <BsFillTelephoneFill className='text-white' />
             </span>
             <p>+63960-609-4407</p>
           </div>
 
           <div className='flex flex-col text-white'>
             <span className='flex items-center gap-x-8'>
-              <BsClock className='text-lg text-rose-300' />
+              <BsClock className='text-lg text-white' />
               <p className='font-medium'>Mon to Sat: <i className='text-sm font-light'>9:00am - 7:00pm</i></p>
             </span>
 
@@ -46,34 +55,10 @@ export const Contacts = () => {
             </span>
           </div>
         </div>
-        <div className='flex gap-x-6 text-white items-center justify-center pt-16 text-xl'>
-            <a href='https://www.facebook.com/MaChristineDentalClinic' className='hover:rounded-full hover:bg-rose-300 p-4'><FaFacebookF /></a>
-            <a href='https://www.instagram.com/mcdclinic' className='hover:rounded-full hover:bg-rose-300 p-4'><BsInstagram /></a>
+        <div className='flex gap-x-6 text-white items-center justify-center py-10 text-2xl'>
+            <a href='https://www.facebook.com/MaChristineDentalClinic' className='border-2 border-white rounded-full hover:bg-white hover:text-rose-300 p-4'><FaFacebookF /></a>
+            <a href='https://www.instagram.com/mcdclinic' className='border-2 border-white rounded-full hover:bg-white hover:text-rose-300 p-4'><BsInstagram /></a>
         </div>
-      </div>
-
-      <div className='basis-2/3 h-full flex flex-col justify-center w-full pt-16 bg-rose-300'>
-        <form className='px-16 space-y-8'>
-            <label className='flex lg:flex-row sm:flex-col items-center gap-x-4 sm:gap-y-2'>
-              <span className='lg:text-4xl sm:hidden lg:block text-white'><AiOutlineUser /></span>
-              <input type="text" placeholder='First Name' className='p-4 basis-1/2 sm:w-full rounded-md focus:outline-none focus:border-2 focus:border-acsent'/>
-              <input type="text" placeholder='Last Name' className='p-4 basis-1/2 sm:w-full rounded-md focus:outline-none focus:border-2 focus:border-acsent'/>
-            </label>
-
-            <label className='flex items-center gap-x-4'>
-              <span className='lg:text-4xl sm:hidden lg:block text-white'><AiOutlineMail /></span>
-              <input type="email" placeholder='Email' className='p-4 basis-full rounded-md focus:outline-none focus:border-2 focus:border-acsent' />
-            </label>
-
-            <label className='flex items-center gap-x-4'>
-              <textarea name="" id="" className='resize-none basis-full rounded-md focus:outline-none focus:border-2 focus:border-acsent p-2 h-36' placeholder='Type your concern/s here.'></textarea>
-            </label>
-
-            <button type='submit' className='uppercase w-full bg-white py-5 rounded text-second font-bold text-2xl hover:bg-acsent hover:text-white flex items-center justify-center gap-x-2 font-pop'>
-              <span><BsFillSendFill /></span>
-              Submit
-            </button>
-          </form>
       </div>
     </div>
   )
