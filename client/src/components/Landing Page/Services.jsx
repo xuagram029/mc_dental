0cb6eda0847422d246f5eb2b60f6f70b96bdae71
Allen Abad <84323269+yowallen@users.noreply.github.com>
2023-05-31 08:30:56 +0800

    Services Added in Landing Page

---
+++ client/src/components/Landing Page/Services.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import service1 from '../../assets/Services/service1.jpg'
+import service2 from '../../assets/Services/service2.jpg'
+import service3 from '../../assets/Services/service3.jpg'
+import service4 from '../../assets/Services/service4.jpg'
+import service5 from '../../assets/Services/service5.jpg'
+
+
+export const Services = () => {
+  return (
+    <>
+        <div className="mb-8 text-center">
+            <h2 className="text-4xl font-pop font-bold text-primary py-4">Our Services</h2>
+        </div>
+
+        <div className="lg:grid lg:grid-cols-3 lg:gap-x-4 lg:pb-8 lg:px-4">
+            <div className="glassV2 p-4 text-gray-800 rounded-lg shadow-md bg-slate-200">
+                <div className="mb-2">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="w-64 overflow-hidden">
+                            <img src={service3} alt="img"
+                                className="object-cover object-center w-full h-full" />
+                        </div>
+                    </div>
+                    <p className="mb-2 text-center text-gray-600 ">
+                        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
+                    </p>
+                </div>
+            </div>
+
+            <div className="glassV2 p-4 text-gray-800 rounded-lg shadow-md bg-slate-200">
+                <div className="mb-2">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="w-64 overflow-hidden">
+                            <img src={service1} alt="img"
+                                className="object-cover object-center w-full h-full" />
+                        </div>
+                    </div>
+                    <p className="mb-2 text-center text-gray-600 ">
+                        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
+                    </p>
+                </div>
+            </div>
+            <div className="glassV2 p-4 text-gray-800 rounded-lg shadow-md bg-slate-200">
+                <div className="mb-2">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="w-64 overflow-hidden">
+                            <img src={service2} alt="img"
+                                className="object-cover object-center w-full h-full" />
+                        </div>
+                    </div>
+                    <p className="mb-2 text-center text-gray-600 ">
+                        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className="lg:grid lg:grid-cols-2 lg:gap-x-4 pb-8 lg:mx-72">
+            <div className="glassV2 p-4 text-gray-800 rounded-lg shadow-md bg-slate-200 px-6">
+                <div className="mb-2">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="w-64 overflow-hidden">
+                            <img src={service4} alt="img"
+                                className="object-cover object-center w-full h-full" />
+                        </div>
+                    </div>
+                    <p className="mb-2 text-center text-gray-600 ">
+                        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
+                    </p>
+                </div>
+            </div>
+            <div className="glassV2 p-4 text-gray-800 rounded-lg shadow-md bg-slate-200 px-6">
+                <div className="mb-2">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="w-64 overflow-hidden">
+                            <img src={service5} alt="img"
+                                className="object-cover object-center w-full h-full" />
+                        </div>
+                    </div>
+                    <p className="mb-2 text-center text-gray-600 ">
+                        " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
+                    </p>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import mcLogo from '../assets/mcLogo.png'
 import heropic from '../assets/heroImg.jpg'
+import { AboutUs } from '../components/Landing Page/AboutUs'
+import { Services } from '../components/Landing Page/Services'
+import { Contacts } from '../components/Landing Page/Contacts'
 
 const Landing = () => {
   return (
@@ -14,12 +17,12 @@ const Landing = () => {
                     <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white">
                     </span>
                 </a>
-                <a href="#" class="group transition duration-300">
+                <a href="#services" class="group transition duration-300">
                     Services
                     <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white">
                     </span>
                 </a>
-                <a href="#" class="group transition duration-300">
+                <a href="#contacts" class="group transition duration-300">
                     Contacts
                     <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-white">
                     </span>
@@ -35,24 +38,28 @@ const Landing = () => {
                 <h1 className='text-white uppercase lg:text-8xl sm:text-4xl font-pop font-medium'>
                     Smile, It lets your teeth breathe
                 </h1>
-                <p className='text-white font-lora font-thin text-justify py-8 opacity-80'>
+                <p className='text-white font-lora font-thin text-justify pt-8 pb-16 opacity-80'>
                 At MC Dental Clinic, we are dedicated to providing exceptional dental care and creating beautiful, healthy smiles. With our team of highly skilled dental professionals and state-of-the-art facilities, we strive to make every visit a comfortable and rewarding experience.
                 </p>
-                <button className="bg-second rounded text-white font-pop text-xl py-4 px-8 font-bold hover:bg-acsent shadow-lg">
+                <a href='register' className="bg-second rounded text-white font-pop text-xl py-4 px-8 font-bold hover:bg-acsent shadow-lg">
                     Register Now!
-                </button>
+                </a>
             </div>
             <div className='basis-1/2 lg:block sm:hidden'>
                 <img src={heropic} alt="hero image" className='rounded-full w-9/12 mx-auto border-4 border-white' />
             </div>
         </section>
       </div>
-      <div id='about' className='h-screen bg-white'>
-
+      <div id='about' className='h-screen bg-second'>
+        <AboutUs />
       </div>
 
-      <div id='services' className='h-screen bg-second'>
+      <div id='services' className='h-full bg-acsent'>
+        <Services />
+      </div>
 
+      <div id='contacts' className='h-screen bg-white'>
+        <Contacts />
       </div>
     </div>
   )
