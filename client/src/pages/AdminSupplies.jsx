5c1f238414874e3c935dd624cbea2262f68047e8
Allen Abad <84323269+yowallen@users.noreply.github.com>
2023-06-05 01:01:39 +0800

    10k

--- client/src/pages/AdminSupplies.jsx
+++ client/src/pages/AdminSupplies.jsx
@@ -3,8 +3,25 @@ import AdminNavbar from '../components/AdminNavbar'
 import Supplies from '../components/Supplies'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from '../context/AuthContext';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AdminSupplies = () => {
+  const { user, dispatch } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+      if(!user){
+        navigate('/admin-login')
+      }else if(user?.resp[0]?.role === 'dentist'){
+        navigate('/dentist-dashboard')
+      }else if(user?.resp[0]?.role === 'patient'){
+        navigate('/patient-dashboard')
+      }
+    }, [user]) 
+
+
   return (
     <div>
       <AdminNavbar />
--- client/src/pages/DentistSupplies.jsx
+++ client/src/pages/DentistSupplies.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import DentistNavBar from '../components/DentistNavBar'
 import Supplies from '../components/Supplies'
+import { AuthContext } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
 
 const DentistSupplies = () => {
+  const { user, dispatch } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+      if(!user){
+        navigate('/dentist-login')
+      }else if(user?.resp[0]?.role === 'admin'){
+        navigate('/admin-dashboard')
+      }else if(user?.resp[0]?.role === 'patient'){
+        navigate('/patient-dashboard')
+      }
+    }, [user]) 
+
+
   return (
     <div>
       <DentistNavBar />
